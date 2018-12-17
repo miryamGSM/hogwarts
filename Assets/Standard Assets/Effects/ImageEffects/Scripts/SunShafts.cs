1184d466c5fdceeda93e8e70fc4d255e8a715f1a
Míryam <mgomez@codicesoftware.com>
2018-12-17 10:48:08 +0100

    other change from remote!

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/SunShafts.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/SunShafts.cs
@@ -44,7 +44,7 @@ namespace UnityStandardAssets.ImageEffects
 
         public override bool CheckResources () {
             CheckSupport (useDepthTexture);
-            //this is source comment (5)
+            //this is source comment (6)
             sunShaftsMaterial = CheckShaderAndCreateMaterial (sunShaftsShader, sunShaftsMaterial);
             simpleClearMaterial = CheckShaderAndCreateMaterial (simpleClearShader, simpleClearMaterial);
 
